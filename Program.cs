275dc594892ea6b038ca5e694cdec37095c9992d
Nikita <smiels@vk.com>
2022-07-23 20:13:45 +0300

    added completed program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿void PrintArray(string[] arr)
+{
+    foreach (string element in arr)
+    {
+        Console.Write($"'{element}' ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = { "hello", "2", "world", ":-)" };
+
+Console.Write($"input array is "); PrintArray(array);
+
+string[] ArrayMethod(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        string temp = array[i];
+        if (temp.Length <= 3) newArray[i] = temp;
+    }
+    return newArray;
+}
+
+string[] newArray = ArrayMethod(array);
+newArray = newArray.Where(x => x != null).ToArray(); 
+Console.Write($"output array is "); PrintArray(newArray);
